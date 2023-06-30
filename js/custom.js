5dd38fcdefc7f87bf27ec66c11d557a8cf76de27
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-06-30 12:47:02 +0900

    반응형 기본 (제이쿼리 빼고)
    반응형 기본 (제이쿼리 빼고)

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,3 @@
-
-// function option () {
-
-// } 최상위로 끌어올려주는 선언. 호이스팅
-
-
-
 const MainSlide = new Swiper('.main_slide', {
     loop: true,
 });
@@ -13,21 +6,55 @@ const leftArrow = document.querySelector('#MainVisual .arrows .left');
 leftArrow.addEventListener('click', function () {
     MainSlide.slidePrev();
 });
+
 const rightArrow = document.querySelector('#MainVisual .arrows .right');
 rightArrow.addEventListener('click', function () {
     MainSlide.slideNext();
 });
 
 
-
-const ProSlide = new Swiper('.pro_slide', {
+const proSlide = new Swiper('.pro_slide', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     spaceBetween: 30,
     pagination: {
         el: ".sbar",
         type: "progressbar",
     },
 
+    breakpoints: {
+        540: {
+            slidesPerView: 4,
+        }
+    }
+
+    // scrollbar: {
+    //     el: ".drag",
+    //     draggable: true,
+    //     dragSize: 150,
+    //     hide: false,
+    // },
+
 });
+
+
+const mobileBtn = document.querySelector('.mobile_btn');
+const GNB = document.querySelector('#gnb');
+const H1 = document.querySelector('h1');
+
+mobileBtn.addEventListener('click', function () {
+    GNB.classList.toggle('on');
+    H1.classList.toggle('on');
+});
+
+
+GNB.addEventListener('wheel', function (e) {
+    if (GNB.classList.contains('on')) {
+        e.preventDefault();
+    }
+})
 
